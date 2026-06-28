919e58da2d17243357273d9a4089bba10889b5bf
ericg2 <l.ittlelettere@gmail.com>
2026-06-28 18:37:32 -0400

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#[warn(missing_docs)]
+mod config;
+mod layers;
+mod macros;
+mod quota;
+mod retry;
+mod throttle;
+mod vfs;
+
+pub use {
+    config::*,
+    layers::ReadOnlyLayer,
+    quota::{MemoryTracker, QuotaLayer, QuotaTracker},
+    retry::RetrySetting,
+    throttle::Throttle,
+};
